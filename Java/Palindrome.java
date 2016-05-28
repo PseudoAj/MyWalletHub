3d776ac46da6800d107f0f2e91cd621946d9cd21
Ajay Krishna Teja Kavuri <ajaykrishnateja@gmail.com>
2016-05-27 20:00:45 -0400

    Dumb solution to palindrome

--- Java/Palindrome.java
+++ Java/Palindrome.java
@@ -4,7 +4,27 @@
 */
 class Palindrome
 {
-  public static void main(String[] args) {
+  //Main string to be tested for a palindrome pattern
+  String checkMeStr;
+
+  //Instantiate the clasws
+  Palindrome(String caseStr)
+  {
+    this.checkMeStr=caseStr;
+  }
+
+  //dumb method to check for Palindrome
+  public boolean isPalindrome()
+  {
+    return this.checkMeStr.equals(new StringBuilder(this.checkMeStr).reverse().toString());
+  }
+
+  //Main methid to create the object and check for the functionality
+  public static void main(String[] args)
+  {
+    Palindrome myTest=new Palindrome("ajay");
+    System.out.println(myTest.isPalindrome());
+
   }
 
 }
